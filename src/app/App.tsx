b45b9e212dd8644c871e6ac269f6c31f56a39876
Rrmcnv <egor.rumyancev.9494@mail.ru>
2024-08-22 13:19:05 +0500

    fixed typescript package conflict and added initialization files to the app folder

---
+++ src/app/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { CalculatorPage } from '../pages/calculator-page/CalculatorPage';
+
+import GlobalStyles from '../shared/styles/global';
+
+function App() {
+  return (
+    <>
+      <CalculatorPage/>
+      <GlobalStyles />
+    </>
+  );
+}
+
+export default App;
